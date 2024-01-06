deeb557a924369ae70d4f3603502065d286e3277
Sudhanva Narayana <nsudhanva@gmail.com>
2024-01-06 15:27:02 -0500

    bland system image

--- main.py
+++ main.py
@@ -11,23 +11,25 @@ from langchain.schema import SystemMessage
 from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
 from dotenv import load_dotenv
 
-from tools.sql import run_query_tool, list_tables
+from tools.sql import run_query_tool, list_tables, describe_tables_tool
 
 load_dotenv()
 
 chat = ChatOpenAI()
 
 tables = list_tables()
-print(tables)
+
 prompt = ChatPromptTemplate(
     messages=[
-        SystemMessage(content="You are an AI that has access to a SQLite database."),
+        SystemMessage(
+            content=f"You are an AI that has access to a SQLite database.\n {tables}"
+        ),
         HumanMessagePromptTemplate.from_template("{input}"),
         MessagesPlaceholder(variable_name="agent_scratchpad"),
     ]
 )
 
-tools = [run_query_tool]
+tools = [run_query_tool, describe_tables_tool]
 
 agent = OpenAIFunctionsAgent(
     llm=chat,
